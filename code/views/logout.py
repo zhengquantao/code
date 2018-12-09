f97a5b9955f2fe9f10635420d13f53dd14b519bd
zhengquantao <1483906080@qq.com>
2018-12-09 14:14:26 +0800

    $git push origin master –allow-unrelated-histories
    Signed-off-by: zhengquantao <1483906080@qq.com>

--- code/views/logout.py
+++ code/views/logout.py
@@ -4,6 +4,6 @@ logt = Blueprint('logt', __name__)
 
 @logt.route('/logout')
 def logout():
-    print (session['user_info'])
+    print(session['user_info'])
     session.clear()  # 清除session
     return render_template('login.html')
